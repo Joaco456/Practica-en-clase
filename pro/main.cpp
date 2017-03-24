16e4c4905e636b4622b35074e53fe6b8fd479760
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-24 11:29:38 -0500

    practica en clase

---
+++ pro/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   /* float radio;
+    float x;
+    float Phi;
+    Phi = 3.1416;
+
+    cout << "Valor de radio: " << endl;
+    cin >> radio;
+    x = Phi*radio*radio;
+    cout <<"El area del circulo es: " << x << endl;
+
+    float lado;
+    float y;
+
+    cout << "Valor del lado: " << endl;
+    cin >> lado;
+    y = lado*lado;
+    cout <<"El area del cuadrado es: " << y << endl;
+
+    float base;float radio;
+    float x;
+    float Phi;
+    Phi = 3.1416;
+
+    cout << "Valor de ra
+    float altura;
+    float z;
+
+    cout << "El valor de la base es: " << endl;
+    cin >> base;
+    cout << "El valor de la altura es: " << endl;
+    cin >> altura;
+    z = base*altura;
+    cout <<"El area del rectangulo es: " << z << endl;
+    */
+
+    /*int a;
+    int b;
+    int i;
+
+    cout << "Ingrese la primera variable: " << endl;
+    cin >> a;
+    cout << "Ingrese la segunda variable: " << endl;
+    cin >> b;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout << "Cambia b a a: " << a << endl;
+    cout << "Cambia de a a b: " << b << endl;
+    */
+
+    int x;
+    int y;
+
+    cout << "Ingrese numero 1: " << endl;
+    cin >> x;
+    cout << "Ingrese numero 2: " <<endl;
+    cin >> y;
+    if (x != y)
+    if (x > y)
+    {cout << x << " x es mayor que y" << endl;}
+    else
+    {cout << y <<" y es mayor que x" << endl;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
