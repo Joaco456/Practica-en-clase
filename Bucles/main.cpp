97d41f2b572781b42303c5b010a2ea1afd7d81e8
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-31 11:26:10 -0500

    bucles

---
+++ Bucles/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int score[4];
+    int x;
+
+    cout << "Ingrese la primera posicion: " << endl;
+    cin >> score[0];
+    cout << "Ingrese la segunda posicion: " << endl;
+    cin >> score[1];
+    cout << "Ingrese la tercera posicion: " << endl;
+    cin >> score[2];
+    cout << "Ingrese la cuarta posicion: " << endl;
+    cin >> score[3];
+
+    x = score[0];
+    score[0] = score[3];
+    score[3] = x;
+    x = score[1];
+    score[1] =score[2];
+    score[2] = x;
+
+
+    cout <<score[3]<<endl;
+    cout <<score[2]<<endl;
+    cout <<score[1]<<endl;
+    cout <<score[0]<<endl;
+    */
+   /*out << "Ingrese el tamaño de su arreglo: "<< endl;
+    int x;
+    cin >> x;
+    int score[x];
+    int p;
+    for(int i = 0; i < x; i++){
+        cout << "Ingrese numero: " << endl;
+        cin >> score[i];
+    }
+
+    for(int i = 0,i < (x/2); i++){
+        p = score[i];
+        score[i] = score[x-1-i];
+        score[x-1-i] = p;
+
+    }
+
+    for(int i= 0; i<x ; i++){
+        cout << "Ingrese numero: " << endl;
+        cin >> score[i];
+    }
+    */
+
+
+
+
+
+
+
+
+    return 0;
+}
